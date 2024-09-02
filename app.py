4c3de10f85d984f5d17373854aebf236fffb93e0
Dean Phillips <dephillips1977@gmail.com>
2024-09-02 11:00:31 -0500

    Multiple data sources

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from langchain.prompts import HumanMessagePromptTemplate, AIMessagePromptTemplat
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 
-
 # AP: url =  "https://rss.app/feeds/SyIisu9HESEvayPf.xml"
 
-url = "https://rss.app/feeds/gn8XLqZTeImTjrmW.xml"
-
-feed = fp.parse(url)
+feeds = {
+    "BBC U.S. Election": "https://rss.app/feeds/gn8XLqZTeImTjrmW.xml",
+    "Washington Post Politics": "https://feeds.washingtonpost.com/rss/politics?itid=lk_inline_manual_2"
+}
 
 def fetch_article_contents(url):
     response = requests.get(url)
@@ -45,6 +45,10 @@ def create_prompt_template():
 
                                                       Please also include a summary of how a centrist, or impartial person might view this news.
                                                       
+                                                      Please also rate the original article on a scale between 0 and 100.  0 is ultra left leaning.  100 is ultra right 
+                                                      leaning.50 is perfectly unbiased.  Also include qualifying words such as slightly, moderately, or extremely
+                                                      right or left leaning to help understand the score.
+
                                                       Please use markdown format for your output"""),
             HumanMessagePromptTemplate.from_template("\nArticle: {article}")
         ]
@@ -79,24 +83,44 @@ def main():
         layout="centered"
     )
  
+    feed_name = st.sidebar.selectbox("Select a feed", list(feeds.keys()))
+
+    feed_url = feeds[feed_name]
+
+    feed = fp.parse(feed_url)
+
     chain = get_filter_chain()
 
-    for entry in feed.entries:
+    st.title("PureNews")
 
-        button_state_key = get_article_toggle_session_state_key_name(entry.title)
-        # Create state for button
-        if button_state_key not in st.session_state:
-            st.session_state[button_state_key] = False
+    # st.write([entry.title for entry in feed.entries])
 
-        btn = st.button(f"{entry.title}", use_container_width=True, on_click=toggle_article_opened, args=(f'{entry.title}',))
-        if st.session_state[button_state_key]:
-            bar = st.progress(0)
-            paragraphs = fetch_article_contents(entry.link)
-            bar.progress(50)
-            filtered_article = get_filtered_article(chain, paragraphs)
-            bar.progress(100)
-            st.markdown(filtered_article)
-            st.markdown(f"[Original Article]({entry.link})")
+    processed_titles = set()
+
+    with st.empty():
+        with st.container():
+
+            for entry in feed.entries:
+
+                if entry.title not in processed_titles:
+
+                    button_state_key = get_article_toggle_session_state_key_name(entry.title)
+                    # Create state for button
+                    if button_state_key not in st.session_state:
+                        st.session_state[button_state_key] = False
+
+                    btn = st.button(f"{entry.title}", use_container_width=True, on_click=toggle_article_opened, args=(f'{entry.title}',))
+
+                    processed_titles.add(entry.title)
+
+                    if st.session_state[button_state_key]:
+                        bar = st.progress(0)
+                        paragraphs = fetch_article_contents(entry.link)
+                        bar.progress(50)
+                        filtered_article = get_filtered_article(chain, paragraphs)
+                        bar.progress(100)
+                        st.markdown(filtered_article)
+                        st.markdown(f"[Original Article]({entry.link})")
 
 if __name__ == "__main__":
     main()
