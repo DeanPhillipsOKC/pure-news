4fe928cb1666b87d49edf5f6dc50e5a0fa4f34d5
Dean Phillips <dephillips1977@gmail.com>
2024-09-03 07:45:30 -0500

    made the processed titles set a field instead of a local

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from typing import Set
 import streamlit as st
 from feeds import FeedManager
 from articles import ArticleProcessor
@@ -12,13 +13,28 @@ class PureNewsApp:
         self.feed_manager = FeedManager()
         self.article_processor = ArticleProcessor()
         self.llm_manager = LLMManager()
+        self.processed_titles: Set[str] = set()
 
-    def get_article_toggle_session_state_key_name(self, button_caption):
+    def _get_article_toggle_session_state_key_name(self, button_caption):
         return f'article_opened_{button_caption}'
 
-    def toggle_article_opened(self, button_caption):
-        key = self.get_article_toggle_session_state_key_name(button_caption)
+    def _toggle_article_opened(self, button_caption):
+        key = self._get_article_toggle_session_state_key_name(button_caption)
         st.session_state[key] = not st.session_state[key] 
+
+    def _load_article_insights(self, feed_entry):
+        bar = st.progress(0)
+        paragraphs = self.article_processor.fetch_article_contents(feed_entry.link)
+        bar.progress(50)
+        filtered_article = self.llm_manager.get_filtered_article(paragraphs)
+        bar.progress(100)
+        st.markdown("#### TL/DR")
+        st.markdown(filtered_article.tldr)
+        
+        st.markdown("#### Essential Facts")
+        st.markdown(filtered_article.essential_facts)
+
+        st.markdown(f"[Original Article]({feed_entry.link})")
 
     def main(self):
         st.set_page_config(
@@ -32,16 +48,15 @@ class PureNewsApp:
         feed = self.feed_manager.get_feed(feed_name)
 
         st.title("PureNews")
-        processed_titles = set()
 
         with st.empty():
             with st.container():
 
                 for entry in feed.entries:
 
-                    if entry.title not in processed_titles:
+                    if entry.title not in self.processed_titles:
 
-                        button_state_key = self.get_article_toggle_session_state_key_name(entry.title)
+                        button_state_key = self._get_article_toggle_session_state_key_name(entry.title)
                         # Create state for button
                         if button_state_key not in st.session_state:
                             st.session_state[button_state_key] = False
@@ -53,23 +68,12 @@ class PureNewsApp:
                                     justify-content: left;
                                 }"""
                         ):
-                            btn = st.button(f"{entry.title}", use_container_width=True, on_click=self.toggle_article_opened, args=(f'{entry.title}',))
+                            btn = st.button(f"{entry.title}", use_container_width=True, on_click=self._toggle_article_opened, args=(f'{entry.title}',))
 
-                        processed_titles.add(entry.title)
+                        self.processed_titles.add(entry.title)
 
                         if st.session_state[button_state_key]:
-                            bar = st.progress(0)
-                            paragraphs = self.article_processor.fetch_article_contents(entry.link)
-                            bar.progress(50)
-                            filtered_article = self.llm_manager.get_filtered_article(paragraphs)
-                            bar.progress(100)
-                            st.markdown("#### TL/DR")
-                            st.markdown(filtered_article.tldr)
-                            
-                            st.markdown("#### Essential Facts")
-                            st.markdown(filtered_article.essential_facts)
-
-                            st.markdown(f"[Original Article]({entry.link})")
+                            self._load_article_insights(entry)
 
 if __name__ == "__main__":
     app = PureNewsApp()
